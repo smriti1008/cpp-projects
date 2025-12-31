80e6573533cab1b71c72f59c36989ac7c52c18f5
Smriti Singh <smritisingh9118@gmail.com>
2025-12-31 09:56:26 +0530

    Add loop.cpp

---
+++ loop.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+int main()
+  /* int i;
+    for(i=1;i<=5;i++)
+    {
+        cout<<"hello\n";
+    }*/
+  /* int n;
+    cout<<"enter n:";
+    cin>>n;
+    int i;
+    for (i= 2;i<n; i=i+2)
+    {
+        cout<<i;
+    }
+    */
+
+
+    //sum of n natural no square addition
+    /*int sum=0;
+    int i,n;
+    cin>>n;
+    for(i=1;i<=n;i=i+1){
+        sum=sum+i*i;
+    }
+    cout<<sum;*/
+
+    //factorial
+
+   {
+    int fact=1;
+    int i,n;
+    cin>>n;
+    for(i=1;i<=n;i=i+1){
+        fact=fact*i;
+    }
+    cout<<fact; 
+  } 
+
+
+
+
+
+
