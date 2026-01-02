f62e594c9e5d60eeccd154372b30663280868f91
Smriti Singh <smritisingh9118@gmail.com>
2026-01-02 07:59:33 +0530

    Add first.cpp

---
+++ first.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+int main()
+/*{
+   cout<<"3*2";
+};*/
+/*
+{
+    //variable declare
+    int a = 10, b = 20;
+    cout<<a <<endl;
+    cout<<b;
+};
+{
+    char c='a';
+    cout<<'a';
+}
+
+{
+    double d = 8.453987126;
+    cout << d;
+};
+
+{
+    //boolean
+    bool b = false;
+    cout<<b;
+}
+
+{
+    int a,b;
+   cin>>a;
+   cin>>b;
+   cout<<a+b;
+
+};
+   */
+  //type casting
+
+  {
+    int a=10;
+    char c='f';
+    a=c;
+    cout<<a;
+  }
